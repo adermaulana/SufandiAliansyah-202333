27622c69ab800f9b13a112bf0f1b7f05ce0c5651
Ade Rahmat Maulana <adermaulana15@gmail.com>
2024-10-28 12:47:49 +0800

    model

--- app/Http/Controllers/HitungController.php
+++ app/Http/Controllers/HitungController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Mahasiswa;
 
 class HitungController extends Controller
 {
@@ -13,6 +14,15 @@ class HitungController extends Controller
         $hasil = $bilang1 + $bilang2;
 
         return view('view_hasiljumlah',[
+            'hasil' => $hasil
+        ]);
+    }
+
+    public function mahasiswa(){
+
+        $hasil = Mahasiswa::all();
+
+        return view('view_mahasiswa',[
             'hasil' => $hasil
         ]);
     }
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/', function () {
 });
 
 Route::get('hitung',[HitungController::class,'hitungduabilangan']);
+Route::get('mahasiswa',[HitungController::class,'mahasiswa']);
